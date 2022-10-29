cc8db63ba49580665a6152535ee7a6b28e838978
lucas_perez <lucas_perez@itrsa.com.ar>
2022-10-29 17:07:39 -0300

    eslint with his rules has added  and also we configured prettier

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
-console.log("====================================");
-console.log("Welcome to the backend template!");
-console.log("====================================");
+console.log('====================================');
+console.log('Welcome to the backend template!');
+console.log('====================================');
 
-import { sumTotal } from "./common";
+import { sumTotal } from './common';
 
 const sales: number[] = [10, 40, 30];
 
 const total = sumTotal(sales);
 
 const message = `Sales January ${total}`;
+
 console.log(message);
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
